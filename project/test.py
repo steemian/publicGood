a1f23205b1b9e75c5c4403e3477222d91a9dc057
steemian <gbd@gmx.com>
2018-01-13 00:55:07 +0100

    TEST case with n rounds

--- project/test.py
+++ project/test.py
@@ -50,6 +50,20 @@ def populateTable():
 	t.play()
 	t.distribute()
 
+def fullTablePlay():
+	players = [] 
+	for i in range(0,20):
+		c = random.choice([BotRandom, BotAllIn, BotHonest, BotBomberman, BotUrchin])
+		players.append(c("P{}".format(i)))
+
+	t = Table(players)
+
+	for round in range(0,3):
+		if (len(t.players) <= 2):
+			break
+
+		t.play()
+		t.distribute()
 
 
 
@@ -58,7 +72,7 @@ def populateTable():
 
 # Run tests
 
-print ("\n------  smokeTest")
-smokeTest()
-print ("\n------  populateTable")
-populateTable()
+#smokeTest()
+#populateTable()
+
+fullTablePlay()
