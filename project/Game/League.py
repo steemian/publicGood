8ba284e609c544059ebb395733900059f9add926
steemian <gbd@gmx.com>
2018-01-16 11:01:27 +0100

    Heavily instrument League for improving dispatch

--- project/Game/League.py
+++ project/Game/League.py
@@ -22,7 +22,7 @@ class League:
 			toreturn += len(t.players)
 		return toreturn
 
-
+		
 	def playRound (self, phaseIndex, roundIndex):
 		print (" -ROUND {}/{} -------".format(roundIndex, Const.ROUNDS_PER_PHASE))
 		for t in self.tables:
@@ -37,15 +37,29 @@ class League:
 		nbHumans = len(self.humans)
 		nbTables = ceil(nbHumans / Const.MAX_HUMANS_PER_TABLE)
 		nbHumansPerTable = nbHumans / nbTables
+#		print ("Dispatch {} humans into {} tables: {} each"
+#			.format(nbHumans, nbTables, nbHumansPerTable))
 
 		shuffle(self.humans)
 		self.tables = []
 		start = 0.
+		popMin = 9999
+		popMax = 0
 
 		for tabIndex in range(0,nbTables):
 			end = start + nbHumansPerTable
 			table = Table(self.humans[round(start):ceil(end)], "Tab {}".format(tabIndex), tabIndex, -1, nbHumans)
 			self.tables.append(table)
 
+#			print ("Table {:2} has {:2} players from {:2.1f} to {:2.1f}"
+#				.format(tabIndex, len(table.players), start, end))
 			start += len(table.players)
 
+			#print("cur={} min={} max={}".format(len(table.players),popMin, popMax))
+			popMin = min(popMin, len(table.players))
+			popMax = max(popMax, len(table.players))
+			#print("cur={} min={} max={}\n\n".format(len(table.players),popMin, popMax))
+
+
+		print ("{:3} humans in {:2} tables (avg={:2.2f}). Min={} Max={}  Delta={}"
+			.format(nbHumans, len(self.tables), nbHumansPerTable, popMin, popMax, popMax-popMin))
